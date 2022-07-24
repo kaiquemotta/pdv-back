a47ae716d9764c1af17de6adb19ffe5f22d60fb8
Kaique Motta <kaique.motta@hotmail.com>
2022-07-24 11:16:17 -0300

    print

--- src/main/java/com/pdv/resources/VendaResource.java
+++ src/main/java/com/pdv/resources/VendaResource.java
@@ -139,7 +139,7 @@ public class VendaResource {
 	//System.out.println(printerService.getPrinters());
 	//		
 	//print some stuff
-	printerService.printString("EPSON", "\n\n testing testing 1 2 3eeeee \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	//printerService.printString("EPSON", "\n\n testing testing 1 2 3eeeee \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
 	// cut that paper!
 	//byte[] cutP = new byte[] { 0x1d, 'V', 1 };
@@ -156,39 +156,13 @@ public class VendaResource {
 
 		    list.add(new WordData("编号: 1001", Align.LEFT, Font.BOLD, 5));
 
-		    list.add(new TitleData("标题"));
-		    list.add(new LineData(true));
-
-		    list.add(new LineData("普通文本", false));
-		    list.add(new WordData("左对齐，超出纸张宽度自动折行，可设置字体样式、对齐方式、字号", Align.LEFT));
-		    list.add(new LineData());
-		    list.add(new WordData("居中对齐，超出纸张宽度自动折行，可设置字体样式、对齐方式、字号", Align.CENTER, Font.PLAIN, Const.FONTSIZE_10));
-		    list.add(new LineData());
-		    list.add(new WordData("右对齐，超出纸张宽度自动折行，可设置字体样式、对齐方式、字号", Align.RIGHT, Font.ITALIC, Const.FONTSIZE_8));
-		    list.add(new SpaceData());
-
-		    list.add(new LineData("键值文本", false));
-		    list.add(new RowData("房间号", "1001"));
-		    list.add(new RowData("入住房型", "豪华大床房"));
-		    list.add(new RowData("入住人", "张三"));
-		    list.add(new RowData("入住日期", "2019-06-20"));
-		    list.add(new RowData("离店日期", "2019-06-21"));
-		    list.add(new RowData("空字段", ""));
-		    list.add(new RowData("备注", "超过纸张宽度折到下一行但标题不会"));
 
 		    list.add(new LineData("二维码", false));
-		    list.add(new QrcodeData("https://www.baidu.com/s?wd=Wow!%20You%20find%20me!"));
 		    list.add(new LineData("本地图片", false));
 
-		    File file = new File("D:\\test.png");
 		    InputStream is;
 		    BufferedImage bi = null;
-		    try {
-		      is = new FileInputStream(file);
-		      bi = ImageIO.read(is);
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }
+		
 		    list.add(new ImageData(bi));
 
 		    list.add(new SpaceData(10));
